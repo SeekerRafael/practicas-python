a4b556412bc28b0219b7c7f34cad4f479610e406
SeekerRafael <rafael.ariasf03@gmail.com>
2025-07-28 20:26:50 -0700

    clases en pytthoin

---
+++ clases.py
@@ -0,0 +1,10 @@
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def saludar(self):
+        print(f"Hola, soy {self.nombre}")
+
+p = Persona("Rafa", 21)
+p.saludar()
--- var.py
+++ var.py
@@ -2,3 +2,6 @@ nombre = "Rafael"
 edad = 21
 pi = 3.1416
 es_estudiante = True
+
+
+

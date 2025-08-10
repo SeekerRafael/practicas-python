f0194e6edb7bae869017b82d2e8ef0df49ca5f11
SeekerRafael <rafael.ariasf03@gmail.com>
2025-08-09 21:44:21 -0700

    impresiones en python

---
+++ Practica-1/print.py
@@ -0,0 +1,18 @@
+print("Me encanta estudiar Python")
+
+print('Estudiar con "Python Total" es super divertido')
+
+print(5+550)
+
+print("Línea 1\nLínea 2\nLínea 3")
+
+print("A\tB\tC\nD\tE\tF\nG\tH\tI")
+
+print("Barra Normal: /\nBarra Invertida: \\")
+
+print(input("¿Qué estás estudiando?"))
+
+print(input("¿En qué país vives?"))
+
+print(input("Escribe tu nombre:")+" "+input("Escribe tu apellido:"))
+

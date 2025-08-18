4c4e5827646752cac6dab93ac2a78e618c80bef3
SeekerRafael <rafael.ariasf03@gmail.com>
2025-08-17 21:29:02 -0700

    convertifor de temperatura

---
+++ practicas/convertiodor.py
@@ -0,0 +1,9 @@
+
+def celsius_a_fahrenheit(c):
+    return (c * 9/5) + 32
+
+def fahrenheit_a_celsius(f):
+    return (f - 32) * 5/9
+
+print(celsius_a_fahrenheit(30))  # 86.0
+print(fahrenheit_a_celsius(100)) # 37.7
